66fb8818fe77642602df8ed4d2cfa55d1343ee12
Momo-87 <momochristian87@gmail.com>
2023-12-30 12:16:53 +0100

    Add textute and color

--- js/main.js
+++ js/main.js
@@ -23,21 +23,31 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 
       // create the shape
-      let geometry = new THREE.BoxGeometry(1, 1, 1);
+      let geometry = new THREE.BoxGeometry(2, 2, 2);
 
-      // create a material, color or image textute
-      let material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, wireframe: true });
+      // create a material, color or image textute for each of the faces of the cube
+      let cubeMaterials = [
+        new THREE.MeshBasicMaterial({ color: 0xFFFFFF, side: THREE.DoubleSide }), //LEFT side with white color face
+        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('./../assets/images/react.png'), side: THREE.DoubleSide }), //RIGHT
+        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('./../assets/images/ruby.png'), side: THREE.DoubleSide }), //TOP
+        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('./../assets/images/node.png'), side: THREE.DoubleSide }), //BOTTOM
+        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('./../assets/images/tailwind.png'), side: THREE.DoubleSide }), //FRONT
+        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('./../assets/images/html.png'), side: THREE.DoubleSide }) //BACK
+      ];
 
-      // create a mesh containing the geometry and material
-      let cube = new THREE.Mesh(geometry, material);
+      // create a material, color, and set the  material wireframe state
+      // let material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, wireframe: true });
+
+      // create a mesh containing the geometry and material properties
+      let cube = new THREE.Mesh(geometry, cubeMaterials);
 
       // add the mesh to the scene
       scene.add(cube);
 
       // position the camera
-      camera.position.z = 3;
+      camera.position.z = 5;
 
-      // game logic
+      // iplement permanent rotation
       let update = () => {
         cube.rotation.x += 0.01;
         cube.rotation.y += 0.005;
